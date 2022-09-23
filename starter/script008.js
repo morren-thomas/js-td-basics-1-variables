25f5bb2306fc591d8ffcdd8e15e7754f86f1bd5f
Morren Thomas <thomas.morren@student.hepl.be>
2022-09-23 15:57:57 +0200

    script008.js

--- starter/script008.js
+++ starter/script008.js
@@ -3,22 +3,24 @@
 */
 
 // 1. Initialiser une variable firstName à John et afficher sa valeur dans la console
-
-
+let firstName = "John";
+console.log(firstName);
 // 2. Initialiser une variable lastName à Smith et une variable age à 28
-
+let lastName = "Smith"
 
 // 3. Initialiser une variable fullAge à true et l'afficher
-
+let fullAge = true;
 
 // 4. Déclarer une variable job et afficher sa valeur dans la console
 
-
+let job;
+console.log(job)
 // 5. Affecter la valeur Enseignat à la variable job et l'afficher 
-
+job = "enseignant"
 
 // 6. Règles de nommage des variables : le nom ne peut pas 
 // - commencer par un chiffre
 // - contenir de caractères spéciaux
 // - être un mot réservé du langage
+
 
